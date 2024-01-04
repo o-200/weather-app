9f9a8217e003baa773e123189422d69a9b99e5d6
Alexander Abramov <o200@192.168.0.10>
2024-01-04 18:44:36 +0700

    added service to interact with weather api, extended column in migration

---
+++ app/services/weather_picker.rb
@@ -0,0 +1,24 @@
+require 'faraday'
+
+class WeatherPicker
+  attr_reader :conn
+
+  API_KEY = Rails.application.credentials.weather_key
+
+  def initialize
+    @conn = Faraday.new(
+      url: 'http://api.weatherapi.com/v1',
+      headers: {'Content-Type' => 'application/json'}
+    )
+  end
+
+  def current_weather(q='Moscow')
+    response = @conn.get("current.json?key=#{API_KEY}&q=#{q}&")
+    body = JSON.parse(response.body)
+
+    {
+      temperature: body['current']['temp_c'],
+      feelslike: body['current']['feelslike_c']
+    }
+  end
+end
--- db/migrate/20240104111854_create_weather_temperatures.rb
+++ db/migrate/20240104111854_create_weather_temperatures.rb
@@ -1,8 +1,9 @@
 class CreateWeatherTemperatures < ActiveRecord::Migration[7.1]
   def change
     create_table :weather_temperatures do |t|
-      t.decimal :temperature
-
+      t.decimal :temp_c
+      t.decimal :feelslike_c
+      
       t.timestamps
     end
   end
