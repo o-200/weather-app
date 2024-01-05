9c4eedffae381f8c9e49f509d42b096b499b43c2
Alexander Abramov <omgout.200@gmail.com>
2024-01-05 19:28:39 +0700

    added weather view without metrics

--- app/controllers/weather_temperatures_controller.rb
+++ app/controllers/weather_temperatures_controller.rb
@@ -1,4 +1,15 @@
 class WeatherTemperaturesController < ApplicationController
   def index
   end
+
+  def create
+    temperature = WeatherPicker.new.current_weather
+    weather = WeatherTemperature.new(temperature)
+
+    if weather.save
+      flash[:notice] = "Record was successfully created"
+    else
+      weather.errors.full_messages
+    end
+  end
 end
