0c03acfcf02d1fcb60833b47acb755b5f481edba
Alexander Abramov <omgout.200@gmail.com>
2024-01-10 18:37:15 +0700

    added seeds for months

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+time_now = Time.now
+
+12.times do |n|
+  10.times do
+    rand_value = rand(-30.0..50).ceil(1)
+    rand_value2 = rand(-5.0..5.0).ceil(1)
+
+    WeatherTemperature.create!(
+      temp_c: rand_value,
+      feelslike_c: rand_value + n,
+      created_at: time_now - n.month
+    )
+  end
+end
+
+puts "All data created!"
