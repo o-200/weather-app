e2bc0485925589288b3bca44b4b8a9792e96b09a
Alexander Abramov <omgout.200@gmail.com>
2024-01-31 23:45:27 +0700

    refactoring and made component

--- app/javascript/controllers/prediction_controller.js
+++ app/javascript/controllers/prediction_controller.js
@@ -1,71 +1,105 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 export default class extends Controller {
-    static targets = ['myChart', 'weatherList'];
+    static targets = ['myChart', 'weatherList', 'weatherHeader'];
+
+    connect() {
+        const chartContext = this.canvasContext();
+        const weatherListTarget = this.weatherListTarget;
+        const weatherHeaderTarget = this.weatherHeaderTarget;
+
+        const avgTemps = gon.temps_by_day.map(hash => hash['avg_temp_c']);
+        const labels = gon.temps_by_day.map(hash => hash['date']);
+        const todayTemp = gon.temps_by_day[0];
+
+        this.renderWeatherHeader(weatherHeaderTarget, todayTemp);
+        this.renderChart(chartContext, labels, avgTemps);
+        this.renderWeatherList(weatherListTarget);
+    }
 
     canvasContext() {
         return this.myChartTarget.getContext('2d');
     }
 
-    connect() { 
-        const avg_temps = gon.temps_by_day.map((hash) => hash['avg_temp_c'])
-        
+    renderWeatherHeader(target, temp) {
+      if (!target) return;
+
+      const header = document.createElement('div');
+      header.id = 'cardsList'
+      header.classList.add('flex', 'justify-between', 'space-x-4', 'dark:text-gray-400');
+      header.innerHTML = `<div class="w-full max-w-screen-ring-white">
+                            <div class="flex justify-between">
+                              <div class="flex flex-col">
+                                <span class="text-6xl font-bold">${temp['avg_temp_c']}°C</span>
+                                <span class="font-semibold mt-1 text-gray-500">Moscow, RU</span>
+                              </div>
+                              <div class="flex flex-col">
+                                <img src='${temp['condition']['icon']}'></img>
+                              </div>
+                            </div>
+                          </div>`;
+
+      target.appendChild(header);
+    }
+
+    renderChart(context, labels, temps) {
         const data = {
-            labels: gon.temps_by_day.map((hash) => hash['date']),
-            datasets: [
-                {
-                    label: 'average temps',
-                    data: avg_temps,
-                    borderColor: '#f67019',
-                    fill: true,
-                    stepped: false,
-                    tension: 0.4,
-                }
-            ]
+            labels: labels,
+            datasets: [{
+                label: 'average temps',
+                data: temps,
+                borderColor: '#f67019',
+                fill: false,
+                stepped: false,
+                tension: 0.4,
+            }]
         };
 
-        new Chart(this.canvasContext(), {
+        new Chart(context, {
             type: 'line',
             data: data,
             options: {
                 responsive: true,
                 interaction: {
-                intersect: false,
-                axis: 'x'
+                    intersect: false,
+                    axis: 'x'
                 },
                 plugins: {
-                legend: {
-                    display: false
-                }
+                    legend: {
+                        display: false
+                    }
                 },
                 scales: {
-                  x: {
-                    display: false,
-                  },
-                  y: {
-                    display: false,
-                  }
+                    x: {
+                        display: false,
+                    },
+                    y: {
+                        display: false,
+                    }
                 }
             }
-            });
+        });
+    }
 
-        if (this.weatherListTarget) {
-          this.weatherListTarget.innerHTML += `<div class="flex justify-between space-x-4 dark:text-gray-400" id="cardsList"></div>`;
+    renderWeatherList(target) {
+        if (!target) return;
 
-          const cardsList = document.getElementById('cardsList');
+        const cardsList = document.createElement('div');
+        cardsList.classList.add('flex', 'justify-between', 'space-x-4', 'dark:text-gray-400');
+        cardsList.id = 'cardsList';
 
-          if (cardsList) {
-            gon.temps_by_day.forEach(element => {
-              cardsList.innerHTML += `<div class="flex flex-col items-center space-y-1">
-                                        <span class="uppercase">${element.date}</span>
-                                        <img src=${element.condition.icon} />
-                                        <span>${element.avg_temp_c}</span>
-                                      </div>`;
-            });
-          }
-        }
+        gon.temps_by_day.forEach(element => {
+            const card = document.createElement('div');
+            card.classList.add('flex', 'flex-col', 'items-center', 'space-y-1');
+            card.innerHTML = `<span class="uppercase">${element.date}</span>
+                              <img src=${element.condition.icon} />
+                              <span>${element.avg_temp_c}</span>`;
 
+            cardsList.appendChild(card);
+        });
+
+        target.appendChild(cardsList);
     }
 }
