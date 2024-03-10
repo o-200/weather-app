d564ae5af9ae461593c9a722e08472d0d3f14d58
Alexander Abramov <omgout.200@gmail.com>
2024-03-10 17:28:24 +0700

    fix routes for prediction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'weather_temperatures#index'
-  get '/prediction', to: 'weather_temperatures#weather_prediction'
+  get '/prediction', to: 'weather_temperatures#weather_prediction', as: 'weather_predictions'
 
   resources :weather_temperatures
 end
